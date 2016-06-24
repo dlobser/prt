42f8bd77472b12892e54bdebc9c40a91ef428673
david lobser <davidlobser@yahoo.com>
2016-06-24 19:44:28 -0400

    added volume control

---
+++ Assets/AngelDust/scripts/VolumeControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VolumeControl : MonoBehaviour {
+
+	AudioSource audi;
+	Vector3 prevPos = Vector3.zero;
+	public float avgAmount = 10;
+	float avg;
+	// Use this for initialization
+	void Start () {
+		audi = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance (transform.position, prevPos);
+		prevPos = transform.position;
+		avg = ((avg * (avgAmount - 1)) + dist) / avgAmount;
+		audi.volume = avg * 5;
+	}
+}
