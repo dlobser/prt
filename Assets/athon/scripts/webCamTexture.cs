1d513c6b381f800c9aa01aa566cc05df2668656d
david lobser <davidlobser@yahoo.com>
2016-09-09 22:18:56 -0400

    checkvideo mat

--- Assets/athon/scripts/webCamTexture.cs
+++ Assets/athon/scripts/webCamTexture.cs
@@ -6,10 +6,22 @@ public class webCamTexture : MonoBehaviour {
 
 	// Use this for initialization
 	void Start() {
-		WebCamTexture webcamTexture = new WebCamTexture();
-		Renderer renderer = GetComponent<MeshRenderer>();
-		renderer.material.mainTexture = webcamTexture;
-		webcamTexture.Play();
+		StartCoroutine (Request ());
+//		WebCamTexture webcamTexture = new WebCamTexture();
+//		Renderer renderer = GetComponent<MeshRenderer>();
+//		renderer.material.mainTexture = webcamTexture;
+//		webcamTexture.Play();
+	}
+
+	IEnumerator Request() {
+		yield return Application.RequestUserAuthorization(UserAuthorization.WebCam | UserAuthorization.Microphone);
+		if (Application.HasUserAuthorization(UserAuthorization.WebCam | UserAuthorization.Microphone)) {
+			WebCamTexture webcamTexture = new WebCamTexture();
+			Renderer renderer = GetComponent<MeshRenderer>();
+			renderer.material.mainTexture = webcamTexture;
+			webcamTexture.Play();
+		} else {
+		}
 	}
 	
 	// Update is called once per frame
